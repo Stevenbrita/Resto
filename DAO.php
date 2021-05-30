b8eedbc3da5ba2927844c0b0d0a950201f86d009
Steven <vanlaethem.steven@gmail.com>
2021-05-30 23:00:24 +0200

    début des annotations de page

--- DAO.php
+++ DAO.php
@@ -1,5 +1,8 @@
 <?php
 
+// Cette page sert de connexion à la base de données qui se trouve sur un serveur local, on y retrouve également les différentes variables qui sont précédées de $ , derrière le = ce sont leurs valeurs.
+// La ligne 13 du code est une déclaration de la variable de connexion // 
+
 $host = "localhost";
 $port = "3306";
 $dbname = "Resto";
