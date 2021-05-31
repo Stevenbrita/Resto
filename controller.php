7258935d289ed0a9ce258eab554f714471bc241d
Steven <vanlaethem.steven@gmail.com>
2021-05-31 03:51:40 +0200

    annotation de le page controller terminée

--- controller.php
+++ controller.php
@@ -2,7 +2,8 @@
 
 // Dans cette page j'ai utilisé la fonction require_once plusieurs fois afin d'inclure les  mêmes fichiers et de les évaluer plusieurs fois durant l'exécution d'un script. Grâce à elle je suis sûr qu'il seront inclus qu'une fois et qu'ils m'éviteront les problèmes de re-déclarations de fonctions ou autres.
 // J'ai également eu recours à la méthode POST qui me permet d'emmagasiner toutes les valeurs "postées" dans un tableau associé, en notant les noms des données du formulaire, car ils représentent les clés dans le tableau associé à cette méthode.
-// Enfin j'ai crée 
+// Enfin j'ai crée une variable RestoAsiatique qui possède les mêmes attributs que la class Resto ainsi que la variable steven qui elle a les mêmes attributs que la class User. 
+// La commande echo m'aide à voir si les informations ont été mises à jour, comme par exemple ce restaurant a bien été ajouté... 
 require_once "Resto.php";
 
 if (isset($_POST['submit'])){
